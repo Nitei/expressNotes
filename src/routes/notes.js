0db32837dc142d776103238c207ee4f171ec404b
Jorge <jorgecampoy81@gmail.com>
2019-06-02 17:07:25 +0200

    Agregadas rutas de usuario y notas

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -2,5 +2,8 @@
 const express = require( 'express' );
 const router = express.Router();
 
+router.get( '/notes', ( req, res ) => {
+  res.send( 'Notes from database' );
+} );
 
 module.exports = router;
--- src/routes/users.js
+++ src/routes/users.js
@@ -2,5 +2,12 @@
 const express = require( 'express' );
 const router = express.Router();
 
+router.get( '/users/signin', ( req, res ) => {
+  res.send( 'Ingresando a la aplicación' );
+} );
+
+router.get( '/users/signup', ( req, res ) => {
+  res.send( 'Formulario de authenticación' );
+} );
 
 module.exports = router;
