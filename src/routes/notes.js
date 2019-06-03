352bddab66f4a7869c8544690cb3b6cd4d0effc6
Jorge <jorgecampoy81@gmail.com>
2019-06-03 12:25:20 +0200

    ++ gestion de errores en las notas

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -10,8 +10,25 @@ router
   } )
   .post( '/notes/new-note', ( req, res ) => {
     console.log( req.body );
-    res.send( req.body );
+    const { title, descripcion } = req.body;
+    const errors = [];
+    if ( !title ) {
+      errors.push( { text: 'Por favor escriba un título' } );
+    }
+    if ( !descripcion ) {
+      errors.push( { text: 'Por favor escriba un descripcion' } );
+    }
+    if ( errors.length ) {
+      res.render( 'notes/new-note', {
+        errors,
+        title,
+        descripcion
+      } );
+    } else {
+      res.send( 'ok' );
+    }
   } );
+
 
 router
   .get( '/notes', ( req, res ) => {
