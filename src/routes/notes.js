ba46e367f37df6477ab283d9859f7c41e18ff68d
Jorge <jorgecampoy81@gmail.com>
2019-06-03 11:58:12 +0200

    ++ notes.js ++ new note.hbs ++ rutas

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,9 +1,21 @@
 // Para crear nuevas notas, actualizarlas, eliminarlas// Irán aquí las urls principales de la aplicación
-const express = require( 'express' );
-const router = express.Router();
+const
+  express = require( 'express' ),
+  router = express.Router()
+  ;
 
-router.get( '/notes', ( req, res ) => {
-  res.send( 'Notes from database' );
-} );
+router
+  .get( '/notes/add', ( req, res ) => {
+    res.render( 'notes/new-note' );
+  } )
+  .post( '/notes/new-note', ( req, res ) => {
+    console.log( req.body );
+    res.send( req.body );
+  } );
+
+router
+  .get( '/notes', ( req, res ) => {
+    res.send( 'Notes from database' );
+  } );
 
 module.exports = router;
